2b5e70e656a92a705544a8f5adc406496075ce29
stasrndn <stas.rndn@yandex.ru>
2022-08-07 22:04:45 +0300

    Добавляет задачу с решением 06-data-types

---
+++ js/06-data-types/main.js
@@ -0,0 +1,29 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+const _string = 'one';
+const _number = 1;
+const _boolean = true;
+const _null = null;
+let _undefined;
+const _object = {};
+const _array = [];
+
+console.log(typeof _string);
+console.log(typeof _number);
+console.log(typeof _boolean);
+console.log(typeof _null);
+console.log(typeof _undefined);
+console.log(typeof _object);
+console.log(typeof _array);
